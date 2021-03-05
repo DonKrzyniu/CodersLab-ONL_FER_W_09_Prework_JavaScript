0bcb7fc8c04d18fc47bade0076d0eb08a7fc6949
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-05 17:12:43 +0100

    zmienne

---
+++ 02_Typy_danych/02_Zmienne/js/app.js
@@ -0,0 +1,24 @@
+// Ta zmienna jest typu number i przechowuje wartość 1906
+const numberValue = 1906;
+console.log(numberValue);
+console.log(typeof numberValue);
+
+//Ta zmienna jest typu string i przechowuje wartość Wisła Kraków
+const stringValue = "Wisła Kraków";
+console.log(stringValue);
+console.log(typeof stringValue);
+
+//Ta zmienna jest typu string i przechowuje wartość 3trzy
+const mixedValue = 3 + "trzy";
+console.log(mixedValue);
+console.log(typeof mixedValue);
+
+//Ta zmienna jest typu boolean i przechowuje wartość true
+const logicValue = true;
+console.log(logicValue);
+console.log(typeof logicValue);
+
+//Ta zmienna jest typu obkect i przechowuje wartość null
+const nullValue = null;
+console.log(nullValue);
+console.log(typeof nullValue);
