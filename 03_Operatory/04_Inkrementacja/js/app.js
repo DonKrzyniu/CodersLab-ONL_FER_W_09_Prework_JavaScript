3cbd15243a7d1c8a1e991a88e95ece158a218b90
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-05 23:12:22 +0100

    inkre

--- 03_Operatory/04_Inkrementacja/js/app.js
+++ 03_Operatory/04_Inkrementacja/js/app.js
@@ -1,3 +1,12 @@
 let startValue;
 let finalValue;
 // Do not change code above!
+
+startValue = 10;
+startValue++;
+
+finalValue = startValue;
+
+console.log(finalValue);
+
+// nie rozumiem komentarza w teście
