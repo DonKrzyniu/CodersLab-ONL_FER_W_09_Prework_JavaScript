40e974ae56c2b02214b2ad3d8472e74329f659bf
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-06 00:43:52 +0100

    switch

--- 04_Kontrola_przeplywu_-_Instrukcje_warunkowe/02_Ocena_literowa/js/app.js
+++ 04_Kontrola_przeplywu_-_Instrukcje_warunkowe/02_Ocena_literowa/js/app.js
@@ -1,1 +1,30 @@
 const grade = 4;
+
+switch (grade) {
+    case 6: {
+        console.log("A");
+        break;
+    }
+    case 5: {
+        console.log("B");
+        break;
+    }
+    case 4: {
+        console.log("C");
+        break;
+    }
+    case 3: {
+        console.log("D");
+        break;
+    }
+    case 2: {
+        console.log("E");
+        break;
+    }
+    case 1: {
+        console.log("F");
+        break;
+    }
+}
+
+//Nie wiem dlaczego test nie widzi wartości "tekstu" i zamiast tego wyskakuje napis: "undefined"
