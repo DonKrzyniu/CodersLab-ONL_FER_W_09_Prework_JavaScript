25a540dfaa5e8abe0590990ab7d34e32b31a9aff
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-05 19:42:30 +0100

    imiona

---
+++ 02_Typy_danych/03_Tablice_z_Imionami/js/app.js
@@ -0,0 +1,9 @@
+const users = ["John", "Marie", "Kate", "Paul", "Steven",];
+
+console.log(users[1]);
+console.log(users[4]);
+console.log(users.length);
+
+
+// 2 część tego zadania jest nie do rozwiązania gdyż nie działa żadne rozwiązanie z materiałów pomocniczych.
+// By nie marnować czasu idę do następnych zadań i być może do tego zadania powrócę.
