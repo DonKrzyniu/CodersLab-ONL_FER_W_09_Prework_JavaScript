f7623ab352161fd99aaa68532b3945af2be06718
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-05 22:52:38 +0100

    porownanie

---
+++ 03_Operatory/03_Porownywanie_zmiennych/js/app.js
@@ -0,0 +1,10 @@
+let numberAsNumber;
+let numberAsString;
+
+let equalValueAndType = numberAsNumber === numberAsString;
+console.log(equalValueAndType);
+
+let equalValue = numberAsNumber == numberAsString;
+console.log(equalValue);
+
+// Według mnie jest OK choć w konsoli mam na czerwono.
