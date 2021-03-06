fae68998c24607d2bbd2a1f26448075e82609ba3
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-07 00:29:37 +0100

    liczby_for

--- 05_Kontrola_przeplywu_-_Petle/02_Liczby_parzyste_i_nieparzyste_-_for/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/02_Liczby_parzyste_i_nieparzyste_-_for/js/app.js
@@ -4,3 +4,10 @@ const n = 13;
  * Write your code below!
  */
 
+for (let n = 0; n <= 13; n = n + 1) {
+    if (n % 2 === 0) {
+        console.log(n + " " + "-" + " " + "even");
+    } else {
+        console.log(n + " " + "-" + " " + "odd");
+    }
+}
