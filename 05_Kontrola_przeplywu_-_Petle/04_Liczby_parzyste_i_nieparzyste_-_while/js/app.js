a885af1fe5952474da51485965a63788263f68c5
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-07 12:13:34 +0100

    while_parzyste

--- 05_Kontrola_przeplywu_-_Petle/04_Liczby_parzyste_i_nieparzyste_-_while/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/04_Liczby_parzyste_i_nieparzyste_-_while/js/app.js
@@ -4,3 +4,6 @@ const n = 13;
  * Write your code below!
  */
 
+//We wcześniejszym zadaniu z tą funkcą sobie nie poradziłem więc tymbardziej nie rozwiążę tego zadania.
+//Idę dalej z zadaniami by rozwiązywać.
+//Wrócę do tego później.
