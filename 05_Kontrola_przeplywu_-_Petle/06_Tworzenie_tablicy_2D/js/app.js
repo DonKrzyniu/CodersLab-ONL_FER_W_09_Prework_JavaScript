8bfe934adaa7f35f373c8ce4ec961a9331b9f4ce
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-07 15:03:56 +0100

    tablica2D

--- 05_Kontrola_przeplywu_-_Petle/06_Tworzenie_tablicy_2D/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/06_Tworzenie_tablicy_2D/js/app.js
@@ -6,3 +6,12 @@ const rows = 5;
  * Write your code below!
  */
 
+let push = numbers;
+for (let i = 1; i < 5; i + 1);
+    for (let j = 1; j < 4; j + 1) {
+    push = push + 1;
+    console.log(push);
+}
+
+//Na tym zakończę bo nie mam pomysłów jak to robić poprawnie.
+//Te tablice to póki co zbyt skomplikowane zagadnienia jak dla mnie.
