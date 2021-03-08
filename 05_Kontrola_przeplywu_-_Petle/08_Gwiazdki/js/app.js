9c595095c1aa8b7a2206a3ba72aefb70736a909e
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-08 19:55:58 +0100

    gwiazdki

--- 05_Kontrola_przeplywu_-_Petle/08_Gwiazdki/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/08_Gwiazdki/js/app.js
@@ -4,7 +4,11 @@ const n = 5;
  * Write your code below!
  */
 for (let i = "*"; i <= n; i++) {
-    console.log(i);
+    for (let j = i; j <= n; j++) {
+        console.log(i + j);
+    }
 }
 
-// brak pomysłów na realizację zadania
+
+
+// brak pomysłów na realizację zadan
