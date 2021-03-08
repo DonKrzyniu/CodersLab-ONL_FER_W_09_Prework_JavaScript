386653021c4cae8da88195c6529e66761e4236a6
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-08 10:11:28 +0100

    while_parzyste

--- 05_Kontrola_przeplywu_-_Petle/04_Liczby_parzyste_i_nieparzyste_-_while/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/04_Liczby_parzyste_i_nieparzyste_-_while/js/app.js
@@ -4,6 +4,13 @@ const n = 13;
  * Write your code below!
  */
 
-//We wcześniejszym zadaniu z tą funkcą sobie nie poradziłem więc tymbardziej nie rozwiążę tego zadania.
-//Idę dalej z zadaniami by rozwiązywać.
-//Wrócę do tego później.
+let i = 0;
+while (i <= n) {
+        if (i % 2 === 0) {
+            console.log(i++ + " " + "-" + " " + "even");
+        } else {
+            console.log(i++ + " " + "-" + " " + "odd");
+        }
+}
+
+//Juppi :-) Udało się :-)
