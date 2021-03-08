0df14ad39725ff51247b28da1fa08607932e14f8
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-08 19:17:10 +0100

    tablica2D

--- 05_Kontrola_przeplywu_-_Petle/06_Tworzenie_tablicy_2D/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/06_Tworzenie_tablicy_2D/js/app.js
@@ -6,9 +6,14 @@ const rows = 5;
  * Write your code below!
  */
 
+numbers[0] = [];
+numbers[1] = [];
+numbers[2] = [];
+numbers[3] = [];
+numbers[4] = [];
 
-for (let i = 1; i <= 5; i = i + 1) {
-        console.log(i);
-    }
 
-//Na więcje mnie nie stać.
+for (let i = 1; i <= 4; i++) {
+    console.log(i);
+}
+
