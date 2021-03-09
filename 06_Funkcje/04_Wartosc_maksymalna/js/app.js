6801b99e401f778b955a039a815ca401606fc73c
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-09 22:52:25 +0100

    max

--- 06_Funkcje/04_Wartosc_maksymalna/js/app.js
+++ 06_Funkcje/04_Wartosc_maksymalna/js/app.js
@@ -3,3 +3,11 @@ const randomNumbers = [27, 64, 47, 78, 48, 11, 76, 25, 11, 83];
 /**
  * Write your code below!
  */
+
+let numbers = randomNumbers;
+
+function maxFromArray(numbers) {
+
+}
+
+//Nie wiem jak się zabrać do tego zadania.
