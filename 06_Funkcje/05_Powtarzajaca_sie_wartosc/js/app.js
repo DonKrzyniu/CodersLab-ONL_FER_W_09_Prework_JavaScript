a09b612384a68a9254c4014be3a08fba585fcc41
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-09 23:18:05 +0100

    powtarzanie

--- 06_Funkcje/05_Powtarzajaca_sie_wartosc/js/app.js
+++ 06_Funkcje/05_Powtarzajaca_sie_wartosc/js/app.js
@@ -3,3 +3,21 @@ const numbers = [2, 4, 5, 2, 3, 5, 1, 2, 4];
 /**
  * Write your code below!
  */
+
+let array = numbers;
+
+function indexOfRepeatedValue(array){
+    let firstIndex = 0;
+    for (let i = 0; i < array.length; i++){
+        for (let j = i + 1; j < array.length; j++){
+            if (array[i] === array[j]);
+            {
+                firstIndex = array.indexOf(array[1]);
+                break;
+            }
+        }
+    }
+    console.log(firstIndex);
+    return firstIndex;
+}
+indexOfRepeatedValue(numbers);
