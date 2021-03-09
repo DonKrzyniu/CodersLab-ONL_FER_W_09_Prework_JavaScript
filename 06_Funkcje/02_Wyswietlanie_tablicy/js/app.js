dbf22aca48a26d430fb7fc9e1c472f39bdaab8ab
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-09 21:08:00 +0100

    wysw_tabl

--- 06_Funkcje/02_Wyswietlanie_tablicy/js/app.js
+++ 06_Funkcje/02_Wyswietlanie_tablicy/js/app.js
@@ -4,6 +4,19 @@ const people = ["John", "Ive", "Donna", "Chris"];
  * Write your code below!
  */
 
+let array = people;
+
+function printArray(array) {
+    for (let i = 1; i < array.length; i++)
+        for (let j = 1; j < array; j++) {
+        console.log(array[i][j]);
+    }
+}
+
+console.log(array);
+printArray(array);
+
+//Zadanie mnie pokonało :-(
 /**
  * Do NOT modify code below!
  */
