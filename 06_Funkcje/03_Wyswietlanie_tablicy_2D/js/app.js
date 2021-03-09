4b545f26f2a103a27310134e1eff8f3cd8d12c6b
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-09 22:33:35 +0100

    wysw_tablica2D

--- 06_Funkcje/03_Wyswietlanie_tablicy_2D/js/app.js
+++ 06_Funkcje/03_Wyswietlanie_tablicy_2D/js/app.js
@@ -4,8 +4,19 @@ const users = [["Jaydn Humphries", "Ayda Orozco"], ["Sanjeev Wilkinson", "Jorge 
  * Write your code below!
  */
 
+let array = users;
 
+function print2DArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            console.log(array[i][j]);
+        }
+    }
 
+console.log(array);
+print2DArray(array);
+
+//Na chwilę obecną nic więcej nie wymyślę.
 
 /**
  * Do NOT modify code below!
