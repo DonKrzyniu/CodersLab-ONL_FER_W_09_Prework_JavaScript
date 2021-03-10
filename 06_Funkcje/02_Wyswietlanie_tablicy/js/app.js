991255f6500587e5e24c5980ae0580bbbf97ad43
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-10 18:42:52 +0100

    wysw_tablicy

--- 06_Funkcje/02_Wyswietlanie_tablicy/js/app.js
+++ 06_Funkcje/02_Wyswietlanie_tablicy/js/app.js
@@ -7,16 +7,13 @@ const people = ["John", "Ive", "Donna", "Chris"];
 let array = people;
 
 function printArray(array) {
-    for (let i = 1; i < array.length; i++)
-        for (let j = 1; j < array; j++) {
-        console.log(array[i][j]);
+    for (let i = 0; i < array.length; i = i + 1){
+        console.log(array[i]);
     }
 }
 
-console.log(array);
 printArray(array);
 
-//Zadanie mnie pokonało :-(
 /**
  * Do NOT modify code below!
  */
