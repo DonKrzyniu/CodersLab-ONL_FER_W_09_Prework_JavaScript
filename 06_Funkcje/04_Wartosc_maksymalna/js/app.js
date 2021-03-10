b2ca5013760db963c97d57758ffc2f92d13a5c60
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-10 20:57:11 +0100

    max

--- 06_Funkcje/04_Wartosc_maksymalna/js/app.js
+++ 06_Funkcje/04_Wartosc_maksymalna/js/app.js
@@ -6,8 +6,13 @@ const randomNumbers = [27, 64, 47, 78, 48, 11, 76, 25, 11, 83];
 
 let numbers = randomNumbers;
 
-function maxFromArray(numbers) {
-
+function maxFromArray(numbers){
+    for (let i = 0; i < numbers.length; i = i + 1);
+    console.log(numbers[i])
 }
 
-//Nie wiem jak się zabrać do tego zadania.
+maxFromArray(numbers){
+    return numbers[i];
+}
+const result = numbers[i];
+console.log(result);
