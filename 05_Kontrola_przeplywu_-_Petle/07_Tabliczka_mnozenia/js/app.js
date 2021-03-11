1341b7b66515c23e4fa6f6e39568b92a90cd0143
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-12 00:04:17 +0100

    mnozenie

--- 05_Kontrola_przeplywu_-_Petle/07_Tabliczka_mnozenia/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/07_Tabliczka_mnozenia/js/app.js
@@ -5,10 +5,35 @@ const calc = [];
  * Write your code below!
  */
 
+let a = 1;
+calc[0] = [
+a *= 1,
+a *= 2,
+a *= 3,
+];
 
-for (let n = 1; n <= 3; n++) {
-    for (let i = 1; i <= 3; i++);
-    console.log(n * i);
-}
+let b = 2;
+calc[1] = [
+b *= 1,
+b *= 2,
+b *= 3,
+];
 
-// brak pomysłu
+let c = 3;
+calc[2] = [
+c *= 1,
+c *= 2,
+c *= 3,
+];
+
+for (let i = 0; i <= calc.length; i++) {
+    for (let j = 0; j <= calc[0].length; j++) {
+        console.log(calc);
+    }
+        }
+
+//calc[0] = ["1 * 1 = 1", "1 * 2 = 2", "1 * 3 = 3"];
+//calc[1] = ["2 * 1 = 2", "2 * 2 = 4", "2 * 3 = 6"];
+//calc[2] = ["3 * 1 = 3", "3 * 2 = 6", "3 * 3 = 9"];
+
+
