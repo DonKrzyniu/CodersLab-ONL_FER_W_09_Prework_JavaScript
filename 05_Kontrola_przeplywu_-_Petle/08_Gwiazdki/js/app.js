14e0d300c18acb38527a3a10898f3936f94305e0
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-11 22:37:28 +0100

    gwiazdki

--- 05_Kontrola_przeplywu_-_Petle/08_Gwiazdki/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/08_Gwiazdki/js/app.js
@@ -4,13 +4,12 @@ const n = 5;
  * Write your code below!
  */
 
-for (let i = 1; i <= n; i = i + 1) {
-    for (let j = 1; j < n; j++) {
-        console.log(i + " " + "*");
+let stars = "*";
+
+for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+        console.log(stars[i][j]);
+    }
 }
-}
 
-
-
-
-
+// Nie mam pojęcia jak inaczej można rozwiązać to zadanie.
