1aff80e2d052ed1e7f26308b2156e2f92e52a295
Andrychowski Krzysztof <79537131+DonKrzyniu@users.noreply.github.com>
2021-03-12 18:31:56 +0100

    maks

--- 06_Funkcje/04_Wartosc_maksymalna/js/app.js
+++ 06_Funkcje/04_Wartosc_maksymalna/js/app.js
@@ -4,15 +4,18 @@ const randomNumbers = [27, 64, 47, 78, 48, 11, 76, 25, 11, 83];
  * Write your code below!
  */
 
-let numbers = randomNumbers;
+let result = randomNumbers;
 
-function maxFromArray(numbers){
-    for (let i = 0; i < numbers.length; i = i + 1);
-    console.log(numbers[i])
+function maxFromArray(result){
+    for (let i = 0; i < result.length; i++) {
+        for (let j = i; j < result.length; j++) {
+            if (result[i] < result[j]){
+                return result;
+            }
+            console.log(result[j]);
+        }
+    }
 }
+maxFromArray(result);
 
-maxFromArray(numbers){
-    return numbers[i];
-}
-const result = numbers[i];
-console.log(result);
+//To zadanie to jednak nie mój poziom!
